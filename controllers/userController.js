092d861061a6b302621837893a9c6b6baef6437e
Tarun Jangra <tarunjangra043@gmail.com>
2024-11-19 12:35:59 +0530

    add More Features

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,6 +15,7 @@ const loginUser = async (req, res) => {
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
+    console.log(isMatch);
 
     if (!isMatch) {
       return res.json({ success: false, message: "Invalid Credentials" });
